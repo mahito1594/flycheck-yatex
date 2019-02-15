4a385e611fefb096dd13de0f274e1fd9a6735414
TANNO Mahito <pb94.mahito@gmail.com>
2019-02-15 23:52:01 +0900

    add templete

--- flycheck-yatex.el
+++ flycheck-yatex.el
@@ -1,4 +1,31 @@
 ;;; flycheck-yatex --- syntax checker for yatex-mode
+
+;; Copyright (C) 2019 Mahito Tanno
+
+;; Author: Mahito Tanno <pb94.mahito@gmail.com>
+;; Package-Requires: ((flycheck "31"))
+;; Keywords: Emacs, TeX/LaTeX, flycheck
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Includes `flycheck.el'
+;; https://www.flycheck.org
+;; Copyright (C) 2017-2018 Flycheck contributors
+;; Copyright (C) 2012-2016 Sebastian Wiesner and Flycheck contributors
+;; Copyright (C) 2013, 2014 Free Software Foundation, Inc.
+;; Released under the GPL v3.0
+
 ;;; Commentary:
 
 ;; This package defines two syntax checkers for yatex-mode with flycheck.
